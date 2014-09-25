040025b5229368e26096d1d01dabc0a791143a25
Anton Yabchinskiy <arn@bestmx.ru>
2014-09-25 17:13:59 +0400

    adjust IS_RATIONAL macro

--- include/rational.hrl
+++ include/rational.hrl
@@ -1,1 +1,6 @@
--define(IS_RATIONAL(Q), is_record(Q, rational, 3)).
+%% For use in guards
+-define(IS_RATIONAL(Q),
+        is_record(Q, rational, 3),
+        is_integer(element(2, Q)),
+        is_integer(element(3, Q))
+       ).
